22576a72689c41c4a59d211cee149b6c43f7e240
Armando Brito <4656134+arbridev@users.noreply.github.com>
2023-05-01 16:43:43 -0400

    Initial Commit

---
+++ Test-GithubActions/Test_GithubActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_GithubActionsApp.swift
+//  Test-GithubActions
+//
+//  Created by Armando Brito on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Test_GithubActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
